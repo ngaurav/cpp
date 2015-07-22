5298b69c903d2e3d4df8b5d8c03c76f004a03585
Nishant Gaurav <nishantg@nishants-macbook-pro.local>
2015-07-22 09:13:18 +0530

    Initial Commit

---
+++ localCPP/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  localCPP
+//
+//  Created by Nishant Gaurav on 22/07/15.
+//  Copyright © 2015 Nishant Gaurav. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
